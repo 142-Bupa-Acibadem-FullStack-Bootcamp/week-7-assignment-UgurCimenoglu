3e6f807e4856b19778694a53a2403e739f2debef
UgurCimenoglu <ugurcimenogluu@gmail.com>
2022-02-04 03:48:42 +0300

    update

--- Currency-Exchange/src/app/app-routing.module.ts
+++ Currency-Exchange/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "home", pathMatch: "full", component: HomeComponent }
+  { path: "", pathMatch: "full", component: HomeComponent }
 ];
 
 @NgModule({
